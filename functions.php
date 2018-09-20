6ade71a50b8186ced69bf02c78c82a2b17fb59a6
Головин Юрий <yuriy.anatolievich@mail.ru>
2018-09-20 10:40:13 +0300

    Errors disabled, white screen

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php function getSumResult( $number ) {
+    $result = ceil( $number );
+    if ( $result > 1000) {
+        $result = number_format( $result, 0, ' ', ' ' ) . ' ₽';
+    }
+    return $result;
+};
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require_once $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+?>
